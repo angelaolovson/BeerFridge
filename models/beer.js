f017f9afbe5f2f02991cadbf867310ef7e5bf5a0
Angela Olovson <angela.olovson@gmail.com>
2023-04-08 16:01:18 -0400

    Created models

---
+++ models/beer.js
@@ -0,0 +1,18 @@
+const {mongoose} = require("../db/connection");
+
+
+const Schema = mongoose.Schema
+
+const beerSchema = new Schema({
+    name: String,
+    brewery: String,
+    price: Number,
+    abv: Number,
+    type: String,
+    notes: String,
+    hops: String
+})
+
+const Beer = mongoose.model("Beer", beerSchema);
+
+module.exports = Beer;
