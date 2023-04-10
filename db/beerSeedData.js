f32ba8336cfdec5f96800d8a3374eeaad5a929d5
Angela Olovson <angela.olovson@gmail.com>
2023-04-10 15:54:56 -0400

    Added seed data

---
+++ db/beerSeedData.js
@@ -0,0 +1,94 @@
+module.exports = [ 
+    {
+        name: "SUMI BABKA",
+        brewery: "Grimm Artisanal Ales",
+        price: 10,
+        abv: 12,
+        type: "imperial milk stout",
+        notes: "cacao, vanilla & sea salt",
+        hops
+    },
+    {
+        name: "SUPER CHANCE",
+        brewery: "Grimm Artisanal Ales",
+        price: 7,
+        abv: 5,
+        type: "dry-hopped gose",
+        notes: "yuzu",
+        hops: "Luminosa and Cashmere"
+    },
+    {
+        name: "MULTIFOLIATE",
+        brewery: "HUDSON VALLEY BREWERY",
+        price: 8,
+        abv: 5,
+        type: "Sour Saison Spritz",
+        notes: "lemon verbena, hibiscus flowers & Cabernet Sauvignon grapes",
+        hops: "Citra"
+    },
+    {
+        name: "PINK LEMONADE SILHOUETTE",
+        brewery: "HUDSON VALLEY BREWERY",
+        price: 10,
+        abv: 5,
+        type: "Brunch-style Sour IPA",
+        notes: "wheat, raspberry & lemon",
+        hops: "Citra"
+    },
+    {
+        name: "AMORPHIA",
+        brewery: "HUDSON VALLEY BREWERY",
+        price: 11,
+        abv: 6,
+        type: "Sour IPA",
+        notes: "wheat & oat, milk sugar, strawberry & vanilla",
+        hops: "Citra & Nelson"
+    },
+    {
+        name: "GLYCERIN",
+        brewery: "HUDSON VALLEY BREWERY",
+        price: 12,
+        abv: 8,
+        type: "Double Fruit Sour DIPA",
+        notes: "wheat & oat, milk sugar, mango & pineapple",
+        hops: "Citra & Mosaic"
+    },    
+    {
+        name: "6TH ANNIVERSARY DDH DOUBLE PILLOW HAT",
+        brewery: "HUDSON VALLEY BREWERY",
+        price: 11,
+        abv: 9,
+        type: "Double Dry Hopped IPA",
+        notes: "wheat",
+        hops: "Citra"
+    }, 
+    {
+        name: "Green Flowers",
+        brewery: "OTHER HALF",
+        price: 10,
+        abv:6.8,
+        type: "West Coast IPA",
+        notes: "Candied pineapple, sweet orange, tropical fruit and some dank creamy notes from the Strata.",
+        hops: "Citra, Amarillo Cryo, Idaho 7 and Strata"
+        },
+    
+    
+    {
+        name: "DDH Forever Ever",
+        brewery: "OTHER HALF",
+        price: 9,
+        abv: 4.7,
+        type: "Double Dry Hopped Session IPA",
+        notes,
+        hops: "Mosaic, Azacca and Citra"
+    },
+    {
+        name: "SOURCE MATERIAL",
+        brewery: "THREE BREWING",
+        price: 8,
+        abv: 4.6,
+        type: "Pacific Northwest Lager",
+        notes: "wildflower bouquet, aged cedarwood, pink peppercorn, touch of berry",
+        hops: "hops from Mt. Hood, Willamette",
+    }
+]
