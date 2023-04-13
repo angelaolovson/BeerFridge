7536dd93ce7d570ad3f1fb7a9fe03e4d1e46739f
Angela Olovson <angela.olovson@gmail.com>
2023-04-13 17:19:20 -0400

    Styled index page and edited seed data

--- db/beerSeedData.js
+++ db/beerSeedData.js
@@ -7,7 +7,8 @@ module.exports = [
         style: "imperial milk stout",
         notes: "cacao, vanilla & sea salt",
         hops:"",
-        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230110/beerproject/sumi-babka-1_kfkr41.png"
+        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681420125/beerproject/sumi-babka-1_elwt3g.png",
+        canImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681419597/beerproject/Cans/sumi-babka-3_myn6y8.png"
     },
     {
         name: "SUPER CHANCE",
@@ -17,7 +18,8 @@ module.exports = [
         style: "dry-hopped gose",
         notes: "yuzu",
         hops: "Luminosa and Cashmere",
-        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230109/beerproject/super-chance-1_bxcfl1.png"
+        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230109/beerproject/super-chance-1_bxcfl1.png",
+        canImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681419598/beerproject/Cans/super-chance-2_r1g2ok.png"
     },
     {
         name: "MULTIFOLIATE",
@@ -27,7 +29,8 @@ module.exports = [
         style: "Sour Saison Spritz",
         notes: "lemon verbena, hibiscus flowers & Cabernet Sauvignon grapes",
         hops: "Citra",
-        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230106/beerproject/MULTIFOLIATE-1_furs4b.jpg"
+        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230106/beerproject/MULTIFOLIATE-1_furs4b.jpg",
+        canImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681419586/beerproject/Cans/MULTIFOLIATE-3_hcbi8y.png"
     },
     {
         name: "PINK LEMONADE SILHOUETTE",
@@ -37,17 +40,19 @@ module.exports = [
         style: "Brunch-style Sour IPA",
         notes: "wheat, raspberry & lemon",
         hops: "Citra",
-        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230108/beerproject/PINK-LEMONADE-SILHOUETTE-1_klzkyp.jpg"
+        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230108/beerproject/PINK-LEMONADE-SILHOUETTE-1_klzkyp.jpg",
+        canImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681419597/beerproject/Cans/PINK-LEMONADE-SILHOUETTE-3_cnyssi.png"
     },
     {
-        name: "AMORPHIA",
+        name: "MIRRORSHIELD",
         brewery: "HUDSON VALLEY BREWERY",
         price: 11,
-        abv: 6,
+        abv: 7,
         style: "Sour IPA",
-        notes: "wheat & oat, milk sugar, strawberry & vanilla",
-        hops: "Citra & Nelson",
-        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230107/beerproject/AMORPHIA-1_ydlpwj.jpg"
+        notes: "wheat & oat, milk sugar, lychee, chamomile & lavender",
+        hops: "Citra & Azacca",
+        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681419791/beerproject/MIRRORSHIELD-1_tlzn4d.jpg",
+        canImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681419582/beerproject/Cans/MIRRORSHIELD-3_iphsav.png"
     },
     {
         name: "GLYCERIN",
@@ -57,38 +62,42 @@ module.exports = [
         style: "Double Fruit Sour DIPA",
         notes: "wheat & oat, milk sugar, mango & pineapple",
         hops: "Citra & Mosaic",
-        showImg: "https://images.squarespace-cdn.com/content/v1/55dbd518e4b054fd5fb51449/1678722380109-LEKZOHAORWN6HR0OTLEA/GlycerinProductPage.jpg?format=2500w"
+        showImg: "https://images.squarespace-cdn.com/content/v1/55dbd518e4b054fd5fb51449/1678722380109-LEKZOHAORWN6HR0OTLEA/GlycerinProductPage.jpg?format=2500w",
+        canImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681419572/beerproject/Cans/GLYCERIN-2_lt5a3k.png"
     },    
     {
-        name: "DDH DOUBLE PILLOW HAT",
+        name: "PILLOW HAT",
         brewery: "HUDSON VALLEY BREWERY",
         price: 11,
         abv: 9,
         style: "Double Dry Hopped IPA",
         notes: "wheat",
         hops: "Citra",
-        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230109/beerproject/PILLOW-HAT-1_qfnxzv.jpg"
+        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230109/beerproject/PILLOW-HAT-1_qfnxzv.jpg",
+        canImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681419597/beerproject/Cans/PILLOW-HAT-3_wq0vhs.png"
     }, 
     {
         name: "GREEN FLOWERS",
-        brewery: "OTHER HALF",
+        brewery: "OTHER HALF BREWING",
         price: 10,
         abv:6.8,
         style: "West Coast IPA",
         notes: "Candied pineapple, sweet orange, tropical fruit and some dank creamy notes from the Strata.",
         hops: "Citra, Amarillo Cryo, Idaho 7 and Strata",
-        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230108/beerproject/Green-Flowers-1_cmzoyd.webp"
+        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230106/beerproject/Green-Flowers-1_flhgq7.png",
+        canImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681419576/beerproject/Cans/Green-Flowers-2_fnhtn4.png"
     },
     
     {
-        name: "DDH FOREVER EVER",
-        brewery: "OTHER HALF",
+        name: "FOREVER EVER",
+        brewery: "OTHER HALF BREWING",
         price: 9,
         abv: 4.7,
         style: "Double Dry Hopped Session IPA",
-        notes:"",
+        notes:"Peach tea, citrus, passion fruit, and mango.",
         hops: "Mosaic, Azacca and Citra",
-        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230109/beerproject/forever-ever-1_kcefwd.png"
+        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681420637/beerproject/forever-ever-1_ejzt6a.png",
+        canImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681419568/beerproject/Cans/forever-ever-2_vkbidz.png"
     },
     {
         name: "SOURCE MATERIAL",
@@ -98,6 +107,7 @@ module.exports = [
         style: "Pacific Northwest Lager",
         notes: "wildflower bouquet, aged cedarwood, pink peppercorn, touch of berry",
         hops: "hops from Mt. Hood, Willamette",
-        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230109/beerproject/SOURCE-MATERIAL-1_xbj7as.webp"
+        showImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681230109/beerproject/SOURCE-MATERIAL-1_xbj7as.webp",
+        canImg:"https://res.cloudinary.com/dlxrcak5o/image/upload/v1681419597/beerproject/Cans/SOURCE-MATERIAL-3_feumtg.png"
     }
 ]
